70b27cf1499c7aeb21591f1c8aafda925721e455
Akihiro Nakamura <30426163+akihiro-n@users.noreply.github.com>
2021-02-09 23:23:44 +0900

    Repository追加
    ・タグ一覧取得
    ・記事一覧取得

---
+++ app/src/main/java/com/example/articlepreview/infra/ArticleRepository.kt
@@ -0,0 +1,19 @@
+package com.example.articlepreview.infra
+
+import com.example.articlepreview.infra.model.Article
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ArticleRepository @Inject constructor(
+    private val qiitaApi: QiitaApi
+) {
+
+    companion object {
+        private const val DEFAULT_PER_PAGE = 20
+    }
+
+    fun getArticles(perPage: Int = DEFAULT_PER_PAGE, page: Int): Flow<List<Article>> = flow {
+        qiitaApi.getArticles(perPage, page)
+    }
+}
---
+++ app/src/main/java/com/example/articlepreview/infra/TagRepository.kt
@@ -0,0 +1,25 @@
+package com.example.articlepreview.infra
+
+import com.example.articlepreview.infra.model.Tag
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class TagRepository @Inject constructor(
+    private val qiitaApi: QiitaApi
+) {
+
+    companion object {
+
+        private const val DEFAULT_PER_PAGE = 20
+
+        /**
+         * タグ一覧を記事数順でソートするクエリ
+         */
+        private const val SORT_QUERY_COUNT = "count"
+    }
+
+    fun getPopularTags(perPage: Int = DEFAULT_PER_PAGE, page: Int): Flow<List<Tag>> = flow {
+        qiitaApi.getTags(perPage, page, SORT_QUERY_COUNT)
+    }
+}
